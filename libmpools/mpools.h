9a49431890db07fab423b38961cdd393cffbe5c6
Peter Simons <simons@cryp.to>
2000-12-16 13:06:03 +0000

    - Added GNU GPL copyright headers. - Cleaned build process up.

--- libmpools/mpools.h
+++ libmpools/mpools.h
@@ -1,11 +1,21 @@
 /*
- *      $Source$
- *      $Revision$
- *      $Date$
- *
- *      Copyright (C) 1996,97 by CyberSolutions GmbH.
- *      All rights reserved.
- */
+   $Source$
+   $Revision$
+
+   Copyright (C) 2000 by CyberSolutions GmbH, Germany.
+
+   This file is part of OpenPetidomo.
+
+   OpenPetidomo is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   OpenPetidomo is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+   General Public License for more details.
+*/
 
 #ifndef __LIB_MPOOLS_H__
 #define __LIB_MPOOLS_H__ 1
--- librfc822/address_scan.h
+++
@@ -1,8 +0,0 @@
-#ifndef YYSTYPE
-#define YYSTYPE int
-#endif
-#define	TOK_ATOM	257
-#define	TOK_ILLEGAL	258
-
-
-extern YYSTYPE rfc822_lval;
