9db74a3e90add2f4e45ea8100e7b3ecb05c704ca
Peter Simons <simons@cryp.to>
2013-02-09 19:42:26 +0100

    bootstrap.sh: update gnulib configuration for release

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,16 +9,19 @@ else
 fi
 
 gnulib_modules=( git-version-gen gitlog-to-changelog gnupload progname
-                 maintainer-makefile announce-gen crypto/md5 )
+                 maintainer-makefile announce-gen stddef crypto/md5 )
 
 $gnulibtool --m4-base build-aux --source-base libgnu --import "${gnulib_modules[@]}"
 
-sed -i -e 's/^sc_prohibit_atoi_atof/disabled_sc_prohibit_atoi_atof/' \
+sed -i -e 's/^sc_bindtextdomain/disabled_sc_bindtextdomain/' \
+       -e 's/sc_cast_of_argument_to_free/disabled_sc_cast_of_argument_to_free/' \
+       -e 's/^sc_prohibit_atoi_atof/disabled_sc_prohibit_atoi_atof/' \
        -e 's/^sc_prohibit_magic_number_exit/disabled_sc_prohibit_magic_number_exit/' \
        -e 's/^sc_prohibit_strcmp/disabled_sc_prohibit_strcmp/' \
-       -e 's/^sc_require_config_h/disabled_sc_require_config_h/' \
-       -e 's/^sc_useless_cpp_parens/disabled_sc_useless_cpp_parens/' \
-   maint.mk
+       -e 's/^sc_prohibit_strcpy/disabled_sc_prohibit_strcpy/' \
+       -e 's/^sc_prohibit_strncpy/disabled_sc_prohibit_strncpy/' \
+       -e 's/^sc_prohibit_test_minus_ao/disabled_sc_prohibit_test_minus_ao/' \
+    maint.mk
 
 build-aux/gitlog-to-changelog >ChangeLog
 
