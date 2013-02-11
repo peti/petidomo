511ad0177843e93deb1724d240b7969db8556ebf
Peter Simons <simons@cryp.to>
2013-02-11 14:35:18 +0100

    maint: post-release administrivia
    * NEWS: Add header line for next release.
    * .prev-version: Record previous version.
    * cfg.mk (old_NEWS_hash): Auto-update.

--- cfg.mk
+++ cfg.mk
@@ -5,4 +5,4 @@ upload_dest_dir_:= /home/frs/project/p/pe/petidomo
 gpg_key_ID      := 99089D72
 today           := $(date "+%Y-%m-%d")
 TAR_OPTIONS     += --mtime=$(today)
-old_NEWS_hash   := 2e454cfbc3073340c89f52987c13e652
+old_NEWS_hash   := c038031744d765d91e18bb4119f69fe0
