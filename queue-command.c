c104e4a16f51501b25eced3abda8a0182ff62727
Peter Simons <simons@cryp.to>
2019-07-10 15:00:47 +0200

    Update copyright headers.

--- queue-command.c
+++ queue-command.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1995-2013 Peter Simons <simons@cryp.to>
+ * Copyright (c) 1995-2019 Peter Simons <simons@cryp.to>
  * Copyright (c) 2000-2001 Cable & Wireless GmbH
  * Copyright (c) 1999-2000 CyberSolutions GmbH
  *
